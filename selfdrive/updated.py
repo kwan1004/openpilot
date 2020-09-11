85b3e1448dc98823fc110b7c8ea089842c48a592
Shane Smiskol <shane@smiskol.com>
2020-09-10 23:32:51 -0500

    trigger an update

--- selfdrive/updated.py
+++ selfdrive/updated.py
@@ -357,7 +357,7 @@ def main():
       params.put("LastUpdateException", f"command failed: {exception.cmd}\n{exception.output}")
 
     # Wait 10 minutes between update attempts
-    wait_helper.sleep(10)  # trigger an update
+    wait_helper.sleep(10)  # trigger an update 2
 
   dismount_ovfs()
 
